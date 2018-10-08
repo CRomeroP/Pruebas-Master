3e5bea082eff3f08a65c3c6630d7f31ad4a644a5
CRomeroP <lituslitus16@gmail.com>
2018-10-08 04:07:51 +0200

    Cambio en los identificadores
    Identificadores cambiados de a,b,c a x,y,z. "This ->" inecesario eliminado

--- Vector3/Project1/main.cpp
+++ Vector3/Project1/main.cpp
@@ -3,15 +3,15 @@
 
 template <class Vector3>
 class Vec3 {
-	Vector3 a, b, c;
+	Vector3 x, y, z;
 public:
 	Vec3()
 	{
-		a = b = c = 0;
+		x = y = z = 0;
 	}
 	Vec3(Vector3 first, Vector3 second, Vector3 third)
 	{
-		a = first; b = second; c = third;
+		x = first; y = second; z = third;
 	}
 	/*Vec3(Vector3 copy)
 	{
@@ -22,7 +22,7 @@ public:
 	void distanceTo(const Vec3<Vector3> &distVec);
 	Vec3<Vector3> operator+(const Vec3<Vector3> &vector) const
 	{
-		return Vec3<Vector3>(a + vector.a, b + vector.b, c + vector.c);
+		return Vec3<Vector3>(x + vector.x, y + vector.y, z + vector.z);
 	}
 	void print();
 };
@@ -31,26 +31,26 @@ template <class Vector3>
 void Vec3<Vector3>::normalize()
 {
 	Vector3 length;
-	length = sqrt((a*a)+(b*b)+(c*c));
+	length = sqrt((x*x)+(y*y)+(z*z));
 	if (length != 0)
 	{
-		this->a = a / length;
-		this->b = b / length;
-		this->c = c / length;
+		x = x / length;
+		y = y / length;
+		z = z / length;
 	}
 	else std::cout << "No se puede normalizar" << std::endl;
 }
 template <class Vector3>
 void Vec3<Vector3>::distanceTo(const Vec3<Vector3> &distVec)
 {
-	float dist = sqrt((distVec.a - this->a)^2 + (distVec.b - this->b)^2 + (distVec.c - this->c)^2);
+	float dist = sqrt((distVec.x - x)^2 + (distVec.y - y)^2 + (distVec.z - z)^2);
 	std::cout << dist << std::endl;
 }
 
 template <class Vector3>
 void Vec3<Vector3>::print()
 {
-	std::cout << a << " " << b << " " << c << std::endl;
+	std::cout << "(" << x << " " << y << " " << z << ")" << std::endl;
 }
 
 int main() {
@@ -60,11 +60,16 @@ int main() {
 	Vec3<int> secondVec(firstVec);
 	Vec3<float> floatVec(2.0, 2.0, 2.0);
 	Vec3<int> sumVec = firstVec + secondVec;
+	std::cout << "Vec3<int> firstVec(1,1,0) -> ";
 	firstVec.print();
+	std::cout << "Vec3<int> secondVec(firstVec) -> ";
 	secondVec.print();
+	std::cout << "Vec3<float> floatVec(2.0,2.0,2.0) Normalizado -> ";
 	floatVec.normalize();
 	floatVec.print();
+	std::cout << "Vec3<int> sumVec = firstVec + secondVec -> ";
 	sumVec.print();
+	std::cout << "Distancia entre vector (1,1,0) y vector (2,1,2) -> ";
 	firstVec.distanceTo(distVec);
 	std::cin.ignore();
 	return 0;
